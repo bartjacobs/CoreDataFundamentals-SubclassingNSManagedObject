214c2f37fa7cc7edcca908036a26004c59fa507a
Bart Jacobs <developer@bartjacobs.com>
2016-01-21 21:17:17 +0100

    Automatically migrate persistent store and infer mapping model

--- Core Data/CoreDataManager.swift
+++ Core Data/CoreDataManager.swift
@@ -32,7 +32,8 @@ class CoreDataManager: NSObject {
         let url = self.applicationDocumentsDirectory.URLByAppendingPathComponent("SingleViewCoreData.sqlite")
         var failureReason = "There was an error creating or loading the application's saved data."
         do {
-            try coordinator.addPersistentStoreWithType(NSSQLiteStoreType, configuration: nil, URL: url, options: nil)
+            let options = [ NSMigratePersistentStoresAutomaticallyOption : true, NSInferMappingModelAutomaticallyOption : true ]
+            try coordinator.addPersistentStoreWithType(NSSQLiteStoreType, configuration: nil, URL: url, options: options)
         } catch {
             // Report any error we got.
             var dict = [String: AnyObject]()
